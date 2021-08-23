711335788e5f5248ed1be2caa7fe606b2e3705b0
DuaLipas <mirzamohyinsks2@gmail.com>
2021-08-23 18:06:57 +0800

    Deleted Everything, we are going to need to undo

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("This Worked");
-
-// Working with APIs
-// Answer the following questions
-// - What's the endpoint (URL)?
-// - What query string/parameters do we need to provide?
-//  -   What are required parameters
-//  -   What are optional parameters do we need to add?
-// -    What HTTP method are we using?
